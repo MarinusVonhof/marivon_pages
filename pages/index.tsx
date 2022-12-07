518af824eb656a3c6754f7419601e137c61a7580
Marinus <marinus.vonhof@live.nl>
2022-12-07 13:34:47 +0100

    image updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image' // Gebruik image-optimization (bereik 100% performance op Lighthouse)
-//import imgLogo from '../public/marivon_logo_300.webp' // gebruik module node-image
+
+// Deze werkte niet bij publiceren via Vercel of Render: "module niet gevonden" (.webp bestand)
+//import imgLogo from '../public/marivon_logo_300x125.webp' // gebruik module node-image
+
 import { Form, Card, Dropdown, Alert, Button, ButtonGroup, OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 export default function Home() {
@@ -11,7 +14,7 @@ export default function Home() {
           <div className="row pt-5">
             <div className="col-lg-8 col-12 mx-auto text-center">
               {/*<Image src={imgLogo} alt="" />*/}
-              <Image src="/marivon_logo_300.webp" alt="" width="500" height="200" />
+              <Image src="../public/marivon_logo_300x125.webp" alt="" width="300" height="125" />
               <h4 className="my-3"><strong>Ontwerpen en toepassen van databases in het semantisch web</strong></h4>
             </div>
           </div>
